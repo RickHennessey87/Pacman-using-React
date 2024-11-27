ea48508902bfff48ddd8b3251d49842d272016fe
Patrick Hennessey <pjhennessey89@gmail.com>
2024-11-27 12:07:27 -0600

    Updated states for game board

--- pacman-game/src/components/GameBoard.js
+++ pacman-game/src/components/GameBoard.js
@@ -1,5 +1,3 @@
-// Manages the state of the game such as positions of Pacman and Ghosts, pellets, score and lives
-
 import React, { useState, useEffect } from 'react';
 import Maze from './Maze';
 import Pacman from './Pacman';
@@ -7,13 +5,20 @@ import Ghost from './Ghost';
 import { mazeLayout } from '../mazeData';
 import './GameBoard.css';
 
+// Manages the state of the game such as positions of Pacman and Ghosts, pellets, score and lives
+
 const GameBoard = () => {
+    // initializes and keeps track on Pacman's movement
     const [pacmanPosition, setPacmanPosition] =useState({ x: 1, y: 1 });
+    // Tracks the direction of Pacman's movement
     const [direction, setDirection] = useState({ x: 0, y: 0 });
+    // Used to dynamically update the state of the maze
     const [maze, setMaze] = useState(mazeLayout);
+    // Keeps track of player score
     const [score, setScore] = useState(0);
 
 
+    // used to detect arrow presses in order to move Pacman. 
     useEffect(() => {
         const handleKeyDown = (e) => {
         switch (e.key) {
@@ -38,14 +43,22 @@ const GameBoard = () => {
         return () => window.removeEventListener('keydown', handleKeyDown);
     }, []);
 
+
+    // Moves Pacman every 200ms based on the direction
+    // This useEffect runs whenever pacmanPosition, direction, maze or score changes 
     useEffect(() => {
+        // changes Pacman's new position buy adding the direction values to pacmanPosition
         const interval = setInterval(() => {
         const newX = pacmanPosition.x + direction.x;
         const newY = pacmanPosition.y + direction.y;
 
+        // Checks to see Pacman's move is valid, i.e. not a wall
         if (maze[newY][newX] !== 1) {
             setPacmanPosition({ x: newX, y: newY });
 
+            // Checks to see if Pacman picked up a pellet, if so
+            // update score by 10 and remove pellet from maze by
+            //changing the space into an empty space
             if (maze[newY][newX] === 2) {
             let newMaze = [...maze];
             newMaze[newY][newX] = 0;
@@ -58,6 +71,8 @@ const GameBoard = () => {
         return () => clearInterval(interval);
     }, [pacmanPosition, direction, maze, score]);
 
+
+    // renders the current state of the game
   return (
     <div className='game-board'>
         <Maze maze={maze} />
